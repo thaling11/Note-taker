ef1901a8fcf6231ae9ff804198a12857f565b7c6
thaling <thaling@verizon.net>
2022-09-06 16:56:03 -0600

    working apiRoutes.js with db_json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,18 +3,18 @@ const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 //GET request
-router.get("/api/notes", (req, res) =>
+router.get("/notes", (req, res) =>
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 );
 
 //POST request
-router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
   const { title, text } = req.body;
 
   if (req.body) {
     const newData = {
-      notetitle,
-      notetext,
+      title,
+      text,
     };
     readAndAppend(newData, "./db/db.json");
 
